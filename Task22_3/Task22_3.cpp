429775a02dcd0a25e20167380cac59dbd16d6d07
DmityFilchev <filchev_dmitry@mail.ru>
2022-09-16 15:42:40 +0300

    Добавьте файлы проекта.

---
+++ Task22_3/Task22_3.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+#include<string>
+#include<map>
+bool anogram(std::string str1,std::string str2)
+{
+    int temp = 1;
+    std::map<char, int> word1,word2;
+    for (auto word_to_str : str1)
+    {
+        word1.insert(std::pair<char, int>(word_to_str, temp));
+    }
+    for (auto word_to_str : str2)
+    {
+        word2.insert(std::pair<char, int>(word_to_str, temp));
+    }
+    return (word1 == word2);
+}
+
+int main()
+{
+    std::string word1;
+    std::string word2;
+    std::cout << "input word 1\n";
+    std::cin >> word1;
+    std::cout << "input word 2\n";
+    std::cin >> word2;
+    if (anogram(word1, word2))
+    {
+        std::cout << "Anogram\n";
+    }
+    else
+        std::cout << "NOT anogram\n";
+}
+
